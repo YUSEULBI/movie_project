9b59065e5b15463f8d8d14013292ee1ae7df5979
YUSEULBI <ha258ppy@gmail.com>
2023-04-25 00:06:57 +0900

    4/25 영화진흥위원회 api 테스트

--- movie/src/main/webapp/js/member/test.js
+++ movie/src/main/webapp/js/member/test.js
@@ -1,6 +1,6 @@
 console.log('test.js연결')
 
-test();
+//test();
 function test(){
 	console.log("test함수실행")
 	$.ajax({
@@ -10,5 +10,35 @@ function test(){
 		success : (r)=>{
 			console.log(r)
 		} 
+	})
+}
+
+//kobisprint();
+function kobisprint(){
+	$.ajax({
+		url : "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"+
+					"?key=e4bef537aa0ae3d7f267d6c813f6cbf3"+
+					"&targetDt=20230423"
+				 ,
+		method : "get" ,
+		success : (r)=>{
+			console.log(r)
+		}
+	})
+}
+
+kobismoviesearch();
+let movielist = [] 
+function kobismoviesearch(){
+	$.ajax({
+		url : "http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=e4bef537aa0ae3d7f267d6c813f6cbf3&openStartDt=2023&itemPerPage=100" ,
+		method : "get" ,
+		success : (r)=>{
+			//console.log(r)
+			//console.log(r.movieListResult.movieList)
+			r.movieListResult.movieList.forEach((m)=>{ if( !m.genreAlt.includes("에로") ){ movielist.push(m)	}	})
+			console.log(movielist)
+		}
+	
 	})
 }
