2afec7c41e24be93b1d71c54f26f0faa77fd1445
YUSEULBI <ha258ppy@gmail.com>
2023-04-25 18:28:14 +0900

    4/25 셀레니움 동적 크롤링 시도중

--- movie/src/main/java/controller/member/Test.java
+++ movie/src/main/java/controller/member/Test.java
@@ -5,11 +5,17 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Test {
 	
@@ -119,14 +125,50 @@ public class Test {
 		
 	} // test end
 	
+	
+	
+	// 셀레니움 동적 크롤링
+	
+	public static final String WEB_DRIVER_ID = "webdriver.chrome.driver"; // 드라이버 ID
+	public static final String WEB_DRIVER_PATH = "C:\\chromedriver.exe"; // 드라이버 경로
+	
 	public void testkofic() throws IOException {
 		System.out.println("testkofic 실행");
-		Document doc = Jsoup.connect("https://kobis.or.kr/kobis/business/mast/mvie/searchMovieList.do?dtTp=movie&dtCd=20112621").get();
-		//System.out.println(doc);
-		Element select = doc.select(".item_tab").first();
-		System.out.println(select);
-		//String src = doc.select(".thumb").select("a").attr("src");
-		//System.out.println(src);
+		
+		// 드라이버 설정
+		try {
+			System.setProperty(WEB_DRIVER_ID, WEB_DRIVER_PATH);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		// 크롬 설정을 담은 객체 생성
+		ChromeOptions options = new ChromeOptions();
+		
+		// 브라우저가 눈에 보이지 않고 내부적으로 돈다.
+		// 설정하지 않을 시 실제 크롬 창이 생성되고, 어떤 순서로 진행되는지 확인 할 수 있다.
+		options.addArguments("headless");
+		
+		// 위에서 설정한 옵션을 담은 드라이버 객체 생성
+		// 옵션을 설정하지 않았을 때에는 생략 가능하다.
+		// WebDriver 객체가 곧 하나의 브라우저 창이라 생각한다.
+		WebDriver driver = new ChromeDriver(options);
+		
+		// 이동을 원하는 URL
+		String url = "https://kobis.or.kr/kobis/business/mast/mvie/searchMovieList.do?dtTp=movie&dtCd=20112708";
+		
+		// WebDriver을 해당 URL로 이동한다.
+		driver.get(url);
+		
+		// 브라우저 이동시 생기는 로드시간을 기다린다.
+		// HTTP응답속도보다 자바의 컴파일 속도가 더 빠르기 때문에 임의적으로 1초를 대기한다.
+		try {Thread.sleep(1000); } catch (InterruptedException e) {	}
+		
+		// class="nav" 인 모든 태그를 가진 WebElement리스트를 받아온다.
+		// WebElement는 html의 태그를 가지는 클래스이다.
+		List<WebElement> el1 = driver.findElements(By.className("thumb"));
+		
+		System.out.println(el1);
 	}
 
 }
